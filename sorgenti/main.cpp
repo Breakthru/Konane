b5a0b4b8938ae3b4d2110da9f1b86343c709f7f2
marco <marco@localhost.localdomain>
2020-04-27 00:57:01 +0100

    new env, new deps

--- sorgenti/main.cpp
+++ sorgenti/main.cpp
@@ -26,7 +26,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 using namespace std;
 #include <iostream>
 #include <sstream>
-#include <gtk/gtk.h>
 //#include "gioco.h"
 #include "konane.cpp"
 #include "interfaccia.cpp"
